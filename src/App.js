88c2dfff109601e56cc4ab044094726ab6aeb704
Ayush Bisht <bisht.ayush2001@gmail.com>
2021-12-19 00:10:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Router } from 'react-router-dom'
 import Home from './pages/home';
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history} basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route  path='/' element={<Home/>}/>        
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
